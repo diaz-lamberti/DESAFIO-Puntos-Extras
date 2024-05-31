eaf090e2b0ebedb5619caedfe82e073dca2d2a5e
mutual <alan.d@centeroffice.com.ar>
2024-05-31 17:11:04 -0300

    ADD: logica para guardar dados

--- DESAFIO-Puntos-Extras.py
+++ DESAFIO-Puntos-Extras.py
@@ -3,12 +3,17 @@ import random
 # Definir clase Dado, con atributos como valor y cara actual
 class Dado():
     valor = []
+
     def tirar(self, cantidad):
+        self.valor.clear()
         for i in range(cantidad):
             self.valor.append(random.randint(1, 6))
 
 # Definir clase Jugador con atributos como nombre, puntaje total y puntaje por ronda
 class Jugador():
+    puntaje_ronda = []
+    puntaje_total = 0
+
     def __init__(self):
         self.nombre = input("Ingresa tu nombre: ")
 
@@ -27,13 +32,25 @@ def definirTurno(jugadores):
 definirTurno(jugadores)
 
 # Definir clase marcador, llevando la cuenta del puntaje de cada jugador y las combinaciones válidas.
-
+        
 # Simular el lanzamiento de cinco dados utilizando un generador de numeros aleatorios
 dado = Dado()
 dado.tirar(5)
 print(dado.valor)
 
+# Logica para guardar dados
+choise = int(input("Cual guardamos?"))
+dados_guardar = []
+for i in range(len(dado.valor)):
+    if choise == dado.valor[i]:
+        dados_guardar.append(dado.valor[i])
+print(dados_guardar)
+
+
 # Proporcionar una interfaz para mostrar las tiradas de dados, las opciones disponibles y el puntaje actual
+rondas = 11
+turnos = 3
+
 
 # Implementar las reglas de puntuación para las diferentes combinaciones posibles
 
